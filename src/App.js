52391450f6d24dbdebbb641712139dd85b4d8aa2
Tomáš Ulman <130840571+TomasUlman@users.noreply.github.com>
2025-03-10 12:20:51 +0100

    Refactoring, loader added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Logo from './components/Logo';
 import Search from './components/Search';
 import PokemonDetail from './components/PokemonDetail';
 import PokemonList from './components/PokemonList';
+import Loader from './components/Loader';
+import { fetchPokemon } from './helpers';
 
 /**
  * Main application component.
@@ -11,14 +13,43 @@ import PokemonList from './components/PokemonList';
  */
 export default function App() {
   const [pokemon, setPokemon] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  /**
+   * Handles setting the current Pokémon by fetching new data.
+   * @param {string|number} nameOrId - The name or ID of the Pokémon to fetch.
+   * @param {Function} setError - Function to set an error message if fetching fails.
+   * @returns {Promise<void>}
+   */
+  async function handleSetPokemon(nameOrId, setError = null) {
+    try {
+      setIsLoading(true);
+      const startTime = Date.now(); // Request start time
+
+      const newPokemon = await fetchPokemon(nameOrId, pokemon);
+
+      const elapsedTime = Date.now() - startTime; // Time duration of request
+      const minLoadTime = 500; // Minimal loading time (500ms)
+
+      setTimeout(() => {
+        setPokemon(newPokemon);
+        setIsLoading(false);
+      }, Math.max(minLoadTime - elapsedTime, 0)); // If 500 ms elapsed, setPokemon immediately
+    } catch (err) {
+      if (setError) setError(err.message);
+      setPokemon(null);
+      setIsLoading(false);
+    }
+  }
 
   return (
     <div>
       <Background />
       <Logo />
-      <Search onSearch={setPokemon} curPokemon={pokemon} />
-      {pokemon && <PokemonDetail curPokemon={pokemon} setPokemon={setPokemon} />}
-      <PokemonList setPokemon={setPokemon} curPokemon={pokemon} />
+      <Search onSearch={handleSetPokemon} curPokemon={pokemon} />
+      {isLoading && <Loader />}
+      {pokemon && !isLoading && <PokemonDetail curPokemon={pokemon} onChangePokemon={handleSetPokemon} />}
+      <PokemonList onChangePokemon={handleSetPokemon} curPokemon={pokemon} />
     </div>
   );
 }
